fb0a443c64f291a465f783a85f17b0b325244844
Johann Yucoco <johahahann@gmail.com>
2018-02-26 12:00:01 +0800

    Changed Dashboard design

--- index.php
+++ index.php
@@ -215,26 +215,17 @@ if($_SESSION['userTypeID'] != 1) {
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-red">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-support fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">13</div>
-                                    <div>Support Tickets!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
+					<ul class="list-group">
+							<li class="list-group-item">First item</li>
+							<li class="list-group-item">Second item</li>
+							<li class="list-group-item">Third item</li>
+							<li class="list-group-item">First item</li>
+							<li class="list-group-item">Second item</li>
+							<li class="list-group-item">Third item</li>
+							<li class="list-group-item">First item</li>
+							<li class="list-group-item">Second item</li>
+							<li class="list-group-item">Third item</li>
+						</ul>
                 </div>
 				<!-- /.col-md-6 -->
             </div>
